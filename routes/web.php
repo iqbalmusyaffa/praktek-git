15e62854b4160e3de8309a84de8c35950c49d74d
iqbalmusyaffa <55908032+iqbalmusyaffa@users.noreply.github.com>
2023-06-15 14:25:40 +0700

    perubahan route dan database migrate

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EmployeeController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProfileController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +19,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('home', [HomeController::class, 'index'])->name('home');
+
+Route::get('profile', ProfileController::class)->name('profile');
+
+Route::resource('employees', EmployeeController::class);
